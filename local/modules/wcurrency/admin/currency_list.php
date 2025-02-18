fd7666e9bdbac52b8437c22209d6f55e1dcb418c
Roman <32770580+wind36@users.noreply.github.com>
2025-02-18 09:52:27 +0300

    Update currency_list.php

--- local/modules/wcurrency/admin/currency_list.php
+++ local/modules/wcurrency/admin/currency_list.php
@@ -1,12 +1,23 @@
 <?php
+// Подключаем пролог административной части до выполнения основной логики
 require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_before.php');
 
+// Используем класс CurrencyTable из модуля WCurrency
 use WCurrency\CurrencyTable;
+use Bitrix\Main\Context;
+
 global $APPLICATION;
+
 $APPLICATION->SetTitle('Список курсов валют');
 
-if ($_REQUEST['action'] === 'delete' && check_bitrix_sessid()) {
-    CurrencyTable::delete($_REQUEST['id']);
+$request = Context::getCurrent()->getRequest();
+
+
+if ($request->get('action') === 'delete' && check_bitrix_sessid()) {
+    $id = $request->get('id');
+    if ($id) {
+        CurrencyTable::delete($id);
+    }
 }
 
 $rsData = CurrencyTable::getList(['order' => ['DATE' => 'DESC']]);
@@ -17,7 +28,12 @@ while ($item = $rsData->fetch()) {
 
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin_after.php');
 
+$listId = 'currency_list';
+
+
 $adminList = new CAdminList($listId);
+
 $adminList->DisplayList();
+
 
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/epilog_admin.php');
