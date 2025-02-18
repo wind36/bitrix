b0c783bd5aefc7d03c4e5f4e94d546dfd9418677
Roman <32770580+wind36@users.noreply.github.com>
2025-02-18 09:59:40 +0300

    Update options.php

--- local/modules/wcurrency/options.php
+++ local/modules/wcurrency/options.php
@@ -1,19 +1,30 @@
 <?php
-
 use Bitrix\Main\Config\Option;
+use Bitrix\Main\Context;
 
+// Идентификатор модуля
 $module_id = 'wcurrency';
 
-if ($REQUEST_METHOD == 'POST' && $save <> '' && check_bitrix_sessid()) {
-    Option::set($module_id, 'update_interval', $_POST['update_interval']);
+$request = Context::getCurrent()->getRequest();
+
+if ($request->isPost() && $request->getPost('save') !== null && check_bitrix_sessid()) {
+    $update_interval = $request->getPost('update_interval');
+    Option::set($module_id, 'update_interval', $update_interval);
 }
 
 $update_interval = Option::get($module_id, 'update_interval', 86400);
-
 ?>
+
+
 <form method="post">
     <?= bitrix_sessid_post() ?>
     <label for="update_interval">Интервал обновления курсов (в секундах):</label>
-    <input type="number" name="update_interval" value="<?= htmlspecialcharsbx($update_interval) ?>">
+    <input 
+        type="number" 
+        id="update_interval" 
+        name="update_interval" 
+        value="<?= htmlspecialcharsbx($update_interval) ?>" 
+        min="1"
+    >
     <button type="submit" name="save">Сохранить</button>
 </form>
